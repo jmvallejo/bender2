eb239aed6c02fe0b74bc0a176f4b45d1e0ca7a90
Juan Manuel Vallejo <jmvallejo@gmail.com>
2018-12-22 17:37:54 -0500

    Add cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import 'dotenv/config'
 import express from 'express'
 import router from './routes'
+import cors from 'cors'
 
 const app = express()
 const port = 3000
+
+// Enable CORS
+app.use(cors())
 
 // Attach router
 app.use('/', router)
