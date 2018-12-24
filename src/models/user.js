dbe1704092f7adb467413bab546bc81de444b3c9
Juan Manuel Vallejo <jmvallejo@gmail.com>
2018-12-23 23:05:15 -0500

    Add test User model

--- src/models/user.js
+++ src/models/user.js
@@ -1,1 +1,9 @@
-// Implement user model
+import { Schema, model } from 'mongoose'
+
+const userSchema = new Schema({
+  username: { type: String, required: true, index: { unique: true } },
+  name: { type: String, required: true },
+  password: { type: String, required: true }
+})
+
+export default model('User', userSchema)
