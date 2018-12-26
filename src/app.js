544c3abcae2893960856daa67b9ce46135404340
Juan Manuel Vallejo <jmvallejo@gmail.com>
2018-12-25 21:02:09 -0500

    Add body-parser dependency

--- src/app.js
+++ src/app.js
@@ -2,11 +2,16 @@ import 'dotenv/config'
 import express from 'express'
 import router from './routes/index'
 import cors from 'cors'
+import bodyParser from 'body-parser'
 
 const app = express()
 
 // Enable CORS
 app.use(cors())
+
+// Enable body parser
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // Attach router
 app.use('/', router)
